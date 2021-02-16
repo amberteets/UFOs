0da1c4506a9f08a058f02d9c1a09d5a1278deb02
Amber Teetsel <amberteetsel@gmail.com>
2021-02-16 13:00:46 -0700

    Code to build table

---
+++ static/js/app.js
@@ -0,0 +1,27 @@
+// import data from data.js
+const tableData = data ;
+
+// Reference the HTML table using d3
+// Tells JavaScript to look for <tbody> tags in the HTML
+var tbody = d3.select("tbody")
+
+// Function to build data table
+function buildTable(data) {
+
+    // Clear existing data
+    tbody.html("") ;
+
+    // Build the table by looping through each object in data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+
+        // Append a row to table body
+        let row = tbody.append("tr");
+
+        // Loop through each field in dataRow and add each value as table cell <td>
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
